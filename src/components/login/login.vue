e8d8da58a868edf526c618cdf803bca71a05e2c7
hst <1772156673@qq.com>
2021-07-21 17:29:07 +0800

    新建登录和组件

---
+++ src/components/login/login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login">
+    
+    <el-form
+      label-position="top"
+      label-width="80px"
+      :model="user"
+      class="box-login"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="user.username" class="login-name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input
+          v-model="user.userpassworld"
+          class="login-passworld"
+        ></el-input>
+      </el-form-item>
+      <el-button type="primary" round class="login-button">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        userpassworld: "",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.login {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+.box-login {
+  width: 400px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+}
+.login-button{
+  width: 100%;
+  margin: 10px 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';//引入对应ui组件
+import 'element-ui/lib/theme-chalk/index.css'; //组件样式，必须引入
+
+import '@/assets/publicstyle/login.css'
 
 Vue.config.productionTip = false
 
