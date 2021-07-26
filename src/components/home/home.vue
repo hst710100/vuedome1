4bcc0cf7f3dd02c9d8a75c5ea404805229821685
hst <1772156673@qq.com>
2021-07-26 15:08:07 +0800

    exit_token

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,13 +1,159 @@
 <template>
-  <div>首页</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <img src="../../assets/93.jpg" alt="图片无法显示" width="90px" />
+        </el-col>
+        <el-col :span="18" class="content">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="2">
+          <a href="#" @click.prevent="exit" class="text">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <el-menu
+          :unique-opened=true
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户列表</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>用户管理</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="2-1">
+                <i class="el-icon-menu"></i>
+                <span>用户管理</span>
+              </el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item index="2-2">
+                <i class="el-icon-menu"></i>
+                <span>角色列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item index="2-3">
+                <i class="el-icon-menu"></i>
+                <span>权限列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>商品列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item index="3-1">
+                <i class="el-icon-menu"></i>
+                <span>分类参数</span>
+              </el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item index="3-2">
+                <i class="el-icon-menu"></i>
+                <span>商品分类</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="4-1">
+                <i class="el-icon-menu"></i>
+                <span>订单列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="5-1">
+                <i class="el-icon-menu"></i>
+                <span>数据报表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  
+  //home组件访问前判断是否有token
+  //beforeCreate  在实例创建以前调用
+  beforeCreate(){
+    //获取本地生成的token
+    const token = localStorage.getItem('token')
+    //判断是否存在，false进入登录组件
+    if (!token) {
+      this.$router.push({name:'login'})
+    }
+  },
+  methods:{
+    exit(){
+      //删除本地生成的token
+      localStorage.clear('token')
+      this.$message.success("用户退出成功");
+      //返回登录组件
+      this.$router.push({name:'login'})
 
-}
+    }
+  }
+};
 </script>
 
 <style>
-
+.container {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+}
+.content {
+  text-align: center;
+  padding: 10px 0;
+}
+.text {
+  text-decoration: none;
+  line-height: 60px;
+}
 </style>
